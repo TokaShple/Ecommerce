3683bca0524c873a63228c82c26a1ad862fcefd1
TokaShple <tokashple@gmail.com>
2023-06-11 01:17:34 +0300

    first commit

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -3,9 +3,10 @@ import { productModel } from "../../../database/models/product.model.js";
 import { cartModel } from "../../../database/models/cart.model.js";
 import catchAsyncError from "../../utlis/middleware/catchAsyncError.js";
 import AppError from "../../utlis/services/AppError.js";
-
+import Stripe from 'stripe';
+const stripe = new Stripe('sk_test_51NHCWaHCjchNe1uLdPwRgf8TAZCotN05E2etXzo8Rwo50hHCcFlM9mMrCxWitGBJ043lCGuNOyfEn6qMH4H1rFPK002S1Svqpq');
 //////////////////////////////////////////////////////////1-CREATE ORDER
-const createOrder=catchAsyncError(async(req,res,next)=>{
+const createCacheOrder=catchAsyncError(async(req,res,next)=>{
   try{
     //1-cart ..... id cart params
     /*let cart=await cartModel.findById(req.params._id);
@@ -61,4 +62,38 @@ const getAllOrder=catchAsyncError(async(req,res,next)=>{
     res.status(500).json({message:"ERROR!!!!!",err});
   }
 })
-export{createOrder,getOrder,getAllOrder}
+//////////////////////////////////////////////////////////4-Create Online Order
+const onlinePayment=catchAsyncError(async(req,res,next)=>{
+  try{
+    //1-cart ..... id cart params
+    const cart = await cartModel.findOne({ user: req.userId });
+    const cartId = req.params._id;
+    //2-total price
+    let totalPrice=cart.totalPriceAfterDiscount?cart.totalPriceAfterDiscount:cart.totalPrice;
+    //STRIPE
+    let session=await stripe.checkout.sessions.create({
+      line_items:[{
+        price_data:{
+          currency:"egp",
+          unit_amount:totalPrice*100,
+          product_data:{
+            name:req.user ? req.user.name : '',
+          },
+        },
+        quantity:1,
+      }],
+      mode:"payment",
+      success_url:"https://route-comm.netlify.app/#/",
+      cancel_url:"https://route-comm.netlify.app/#/cart",
+      customer_email: req.user ? req.user.email : '',
+      client_reference_id:req.params.id,
+      metadata:req.body.shippingAdderss,
+    });
+    res.status(200).json({message:"DONE...",session});
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message:"ERROR!!!!!",err});
+  }
+})
+
+export{createCacheOrder,getOrder,getAllOrder,onlinePayment}
--- src/modules/order/order.routes.js
+++ src/modules/order/order.routes.js
@@ -3,7 +3,9 @@ import * as orderConrtoller from "./order.controller.js";
 import { protectRoutes } from "../auth/auth.controllers.js";
 const orderRouter=Router();
 orderRouter.route("/:id")
-  .post(protectRoutes,orderConrtoller.createOrder);
+  .post(protectRoutes,orderConrtoller.createCacheOrder);
+orderRouter.route("/checkout/:id")
+  .post(protectRoutes,orderConrtoller.onlinePayment);
 orderRouter.route("/")
   .get(protectRoutes,orderConrtoller.getOrder);
 orderRouter.get("/getAllOrders",protectRoutes,orderConrtoller.getAllOrder);
